c95b84fae7c5e165f3001e042224781f0ebabde0
srushti <srushtikathiriya2003@gmail.com>
2024-01-18 08:14:22 +0530

    18-01-2024

---
+++ 6.javascript/introduction/index.js
@@ -0,0 +1,52 @@
+
+{
+    // var number1 = 20;
+    // var Total = number1 + number2;
+    // var number2 = 20;
+    // console.log(number2);
+    // var number2 = 40;
+    // console.log(Total)
+}
+
+/*******New Html Element Add In Though Javascript *********/
+
+{
+    // var newelement = document.createElement("h1");
+    // newelement.textContent="hello world";
+    // document.body.appendChild(newelement);
+    
+    // var newelement=document.createElement("marquee" , Option);
+    // newelement.textContent="hello world";
+    // document.body.appendChild(newelement);
+    
+    // var newelement=document.createElement("pre");
+    // newelement.textContent="how are you!";
+    // document.body.appendChild(newelement);
+}
+
+// Template Literals
+
+{
+    // var _nameone = 20 + 20 + '5';
+    // var _nameone = 20 + 20 +'5'+20 -'5'+10*2;
+    // // var _nametwo= '5';
+    console.log(_nameone);
+    console.log(typeof(_nameone));
+
+
+
+    // var one = 20
+    // var two = 40
+
+    // var three = "one 'two'"
+    // console.log(three);
+
+    // var four  = 'one "two"'
+    // console.log(four);
+
+    // var five = `''one' "two"`
+    // console.log(five);
+
+    // var six = `${one} + ${two}`
+    // console.log(six);
+}
