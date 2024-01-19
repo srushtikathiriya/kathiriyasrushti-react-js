196432c133f1b4a6403f5ae29a9dc9e795a0442d
srushti <srushtikathiriya2003@gmail.com>
2024-01-19 08:10:38 +0530

    19-01-2024

---
+++ 6.javascript/variable/Lj-1.js
@@ -0,0 +1,97 @@
+// Declaration var()
+
+// Global Variables
+
+// var z = 20;
+// {
+//     var x = 30;
+// }
+// {
+//     var y = 50;
+// }
+// console.log(x);
+// console.log(z);
+// console.log(y);
+
+
+// Declaration let()
+// Block-scope variable
+    // {
+        // let x = "Hello"
+        // let x ='java'
+        // x = 'javascript'
+        // console.log(x);
+    // }
+
+
+    
+//  Declaration const() 
+// const x =20;
+// {
+//     const x = 20;
+//     console.log(x);
+// } 
+// {
+//     const x = 50;
+// } 
+// console.log(x);
+
+
+// String with quotes
+
+// {
+//     let string1 = "he'llo"
+//     console.log(string1);
+
+//     let string2 = 'he"llo'
+//     console.log(string2);
+
+//     let string3 = "he\'llo"
+//     console.log(string3);
+
+//     let string4 = 'he\"llo'
+//     console.log(string4);
+
+//     let string5 = "he\nllo"
+//     console.log(string5);
+
+//     let string6 = "he\tllo"
+//     console.log(string6);
+// }
+
+
+
+// {
+    // const x,y;
+    // x = 20 ;     //undefined error
+    // y = 50 ;
+    // console.log(x);
+    // console.log(y);
+
+    // const x,y;
+    // const x = 20 ;     
+    // const y = 50 ;
+    // console.log(x);
+    // console.log(y);
+// }
+
+// {
+    // var x,y;
+    // x = 20;
+    // y = 30;
+    // console.log(x);
+    // console.log(y);
+// }
+
+// {
+//     let x,y;
+//     x = 20;
+//     y = 30;
+//     console.log(x);
+//     console.log(y);
+// }
+
+
+// x = 20 ;
+// window.alert(x);  //defined
+// window.alert(xx); //undefined
