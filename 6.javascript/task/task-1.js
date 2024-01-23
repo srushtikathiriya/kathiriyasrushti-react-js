471b00d9dd4faefe7424ebf6538ac0fa801a5d3d
srushti <srushtikathiriya2003@gmail.com>
2024-01-23 08:13:34 +0530

    23-01-2024

---
+++ 6.javascript/task/task-1.js
@@ -0,0 +1,35 @@
+// 1.
+// {
+//     var x = 1 + 2 * 3
+//     console.log(x);
+//     console.log(typeof(x));
+// }
+// {
+//     var x = 1 + "2" * 3
+//     console.log(x);
+//     console.log(typeof(x));
+// }
+// {
+//     var x = "1" + " 2" *3
+
+//     console.log(x);
+//     console.log(typeof(x));
+// }
+
+// 2.
+// {
+//     var x = 1 + 2**3/2-1
+//     console.log(x);
+// }
+
+// {
+//     var x =  9/2*3
+//     console.log(x);
+// }
+
+// 9.
+// {
+//     var height = 123.56;
+//     var type = (height>=190) ? "tall" : "short";
+//     console.log(type);
+// }
---
+++ 6.javascript/task/task-2.js
@@ -0,0 +1,85 @@
+// que-1
+{
+    // let a = 2;
+
+    // if(a%2==0){
+    //     console.log("number is even");
+    // }
+    // else{
+    //     console.log("number is odd");
+    // }
+
+}
+
+// que-2
+
+// {
+//     let a = 5;
+//     let b = 12;
+//      if(a>b){
+//         console.log("a is larger number");
+//      }
+//      else{
+//         console.log("b is lager number");
+//      }
+// }
+
+// que-4
+{
+    // let marks = 34;
+
+    // if(marks>=95){
+    //     console.log("A grade");
+    // }
+    // else if(marks<95 && marks>=70) {
+    //     console.log("B grade");
+    // }
+    // else if(marks<70 && marks >= 45){
+    //     console.log("C grade");
+    // }
+    // else if(marks < 45 && marks >= 35){
+    //     console.log("D grade");
+    // }
+    // else{
+    //     console.log("You are fail ");
+    // }
+}
+
+
+
+// JAVASCRIPT LOGICAL PROGRAM 
+
+// que-1
+// {
+//     let a = -9
+//     let b = 3
+//     let c = -9*3
+//     console.log(c);
+// }
+{
+//         let a = "value is"
+//         let b = 50
+//         let c = "Value is" + 50 
+//         console.log(c);
+// 
+}
+{
+    let a = 17
+    let b = 5
+    let c = 17 % 5
+    console.log(c);
+}
+{
+    // let a = 5
+    // let b = 17
+    // let c =  5 % 17 
+    // console.log(c);
+}
+
+{
+    // let a = 5
+    // let b = 10
+    // let c =  5 / 10
+    // console.log(c); 
+    
+}
