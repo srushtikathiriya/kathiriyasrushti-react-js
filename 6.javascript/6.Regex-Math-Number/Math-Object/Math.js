66ea327eb4ebfa6bc995cf54d6f263c2c306c718
srushti <srushtikathiriya2003@gmail.com>
2024-02-02 08:18:36 +0530

    02-02-2024

---
+++ 6.javascript/6.Regex-Math-Number/Math-Object/Math.js
@@ -0,0 +1,132 @@
+// Math.E
+
+// let math1 = Math.E    //2.718281828459045
+// console.log(math1);
+
+
+
+// Math.PI
+
+// let math1 = Math.PI      //3.141592653589793
+// console.log(math1);
+
+
+
+// Math.rounded
+
+// let math1 = Math.round(10.5)   //11
+// console.log(math1);
+// let math2 = Math.round(10.45)  //10
+// console.log(math2);
+// let math3 = Math.round(-10.5)    //-10
+// console.log(math3);
+// let math4 = Math.round(-0.6)        //-1
+// console.log(math4);
+
+
+
+// Math.cail
+
+// let math1 = Math.ceil(10.5)   //11
+// console.log(math1);
+// let math2 = Math.ceil(-10.5)     //-10
+// console.log(math2);
+// let math3 = Math.ceil(0.5)      //1
+// console.log(math3);
+
+		
+
+//Math.floor():-
+
+// let math1  = Math.floor(-89.12)   //-90
+// console.log(math1);
+ 
+// let math2  = Math.floor(89.12)    //89
+// console.log(math2);
+
+// let math3 = Math.floor(0)   // 0
+// console.log(math3);
+
+// let math4 =  Math.floor(-0)   //-0
+// console.log(math4);
+
+	
+	
+
+//Math.trunc():-
+
+// let math1  = Math.trunc(10.99999)   //10
+// console.log(math1);
+
+// let math2  = Math.trunc(-10.99999)   //-10
+// console.log(math2);
+
+
+		
+
+//Math.sign():-
+
+// let math1 = Math.sign(-0)    //-0
+// console.log(math1);
+
+// let math2 = Math.sign(-10)   //-1
+// console.log(math2);
+
+// let math3 = Math.sign(10)    //1
+// console.log(math3);
+
+
+//Math.pow():-
+
+// let math1  = Math.pow(5 , -5)    //0.00031999999999999997
+// console.log(math1);
+
+
+// let math2  = Math.pow(5 , 5)      //3125
+// console.log(math2);
+
+
+		
+//Math.sqrt():-
+
+// let math1  = Math.sqrt(0)    //0
+// console.log(math1);
+
+// let number  = -(0-0)-0      //-0
+// console.log(number);
+
+// let math3  = Math.sqrt(625)    //25
+// console.log(math3);
+
+// let math4  = Math.sqrt(-25)      //NaN
+// console.log(math4);
+
+		
+
+//Math.abs():-
+
+// let math1 = Math.abs(-0)   //0
+// console.log(math1);
+
+// let math2 = Math.abs(-10)    //10
+// console.log(math2);
+
+// let math3 = Math.abs(10)   //10
+// console.log(math3);
+
+
+
+//Math.min() and Math.max():-
+
+// let math1  = Math.min(-15  , -155 , 20  , 50  , 230)   //-155
+// console.log(math1);
+
+// let math2  = Math.max(-15  , -155 , 20  , 50  , 230)   //230
+// console.log(math2);
+
+		
+
+//Math.random():-
+
+// const math1 = Math.random()*10000
+// console.log(math1);
---
+++ 6.javascript/task/task-4.js
@@ -0,0 +1,44 @@
+{
+    // var x = 0;
+    // if(10 > 9 > 8)
+    // {
+    //     x += 1;
+    // }
+    // console.log(x);
+}
+
+{
+    // var bar = function foo() {};
+    // console.log(bar === foo);
+}
+
+{
+    // console.log(1 + 2 + '1');    
+    //1 + 2 = 3
+    //3 + '1'
+    //31
+    // console.log('1' + 1 + 2); //112 
+}
+
+{
+    // console.log("9" > "19");   //true
+    // console.log("09" > "19");     //false
+}
+
+{
+    // const f = new Boolean(false)
+    //     if(f){
+    //         console.log(1);
+    //     }else{
+    //         console.log(2);
+    //     }
+    //     console.log(typeof(f));    // 1    // object
+}
+
+{
+    // let x = 100;
+    // let y = x++;
+    // let z = ++x;
+    // let n = (x==y) ? z++ : ++z;
+    // console.log(n);
+}
